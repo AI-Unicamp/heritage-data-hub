166c41944f08fafea2cfd8052809e9f63b8f79e7
Vagner I. de Oliveira <vagner.inol@gmail.com>
2025-02-27 20:26:47 -0300

    Remove europeana_db from Git index to add as a submodule

--- europeana_db/europeana_crawler/config/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from a .env file
-load_dotenv()
-
-# ✅ API Key (stored in an environment variable)
-EUROPEANA_API_KEY = os.getenv("EUROPEANA_API_KEY")
